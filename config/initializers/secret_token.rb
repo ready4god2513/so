96fd43d04f65b29ec90de2fd73e16fbb99afeced
Brandon Hansen <brandonh@ibethel.org>
2014-04-03 21:57:02 -0700

    Base rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+So::Application.config.secret_key_base = '1b69c351062845823dbdf946e5149348cd821d309a311dda60a1f232e5028dff6189f0d522a2e2b5f8feadac79364770067cdae0e45114106dee1bd1d1c9d256'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+So::Application.config.session_store :cookie_store, key: '_so_session'
